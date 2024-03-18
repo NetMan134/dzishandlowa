5ac9b9a7dbb172bbf80af43897817cad17f8f468
NetMan <13informatyka14@gmail.com>
2024-03-19 00:24:16 +0100

    Remove base path for working in inner folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '',
   plugins: [
     vue(),
     VitePWA({
